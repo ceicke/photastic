b02fc4064a01bc193dde6ad48aa66af0976441a6
Christoph Eicke <christoph@geisterstunde.org>
2013-05-14 18:52:38 +0200

    setting more stuff for S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,9 @@ Photastic::Application.configure do
   AWS::S3::DEFAULT_HOST = "s3-eu-west-1.amazonaws.com"
   config.paperclip_defaults = {
     :storage => :s3,
-    :url => ":s3_eu_url",
+    :s3_permissions => :private,
+    :s3_protocol => 'https',
+    :s3_host_name => 's3-eu-west-1.amazonaws.com',
     :s3_credentials => {
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,6 +1,3 @@
 # config/initializers/paperclip.rb 
-Paperclip.interpolates(:s3_eu_url) { |attachment, style|
-  "#{attachment.s3_protocol}://s3-eu-west-1.amazonaws.com/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/}, "")}"
-}
-Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+#Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+#Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
