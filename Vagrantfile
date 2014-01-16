cbfda35bec9a0ef0881ec0a21525897c10d5c34f
Christoph Eicke <christoph@geisterstunde.org>
2014-01-16 09:24:50 +0000

    finalizing the vagrant setup

--- Vagrantfile
+++ Vagrantfile
@@ -13,6 +13,8 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = "ubuntu-13.04"
 
   config.vm.provision :shell, :path => "bootstrap.sh"
+  config.vm.provision :shell, :path => "install-rvm.sh",  :args => "stable"
+
   config.vm.network :forwarded_port, host: 3000, guest: 3000
 
   # The url from where the 'config.vm.box' box will be fetched if it
--- bootstrap.sh
+++ bootstrap.sh
@@ -4,31 +4,5 @@ apt-get -y update
 apt-get -y install build-essential
 apt-get -y install bash curl git patch bzip2 build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev libgdbm-dev ncurses-dev automake libtool bison subversion pkg-config libffi-dev libcurl3-dev imagemagick libmagickwand-dev libpcre3-dev postgresql-9.1 htop ruby-dev postgresql-server-dev-9.1
 
-cd /home/vagrant
-
-\curl -L https://get.rvm.io | bash -s stable
-
-source /home/vagrant/.profile
-source /home/vagrant/.rvm/scripts/rvm
-
-rvm install 2.0.0
-rvm use 2.0.0 --default
-
-gem source -r http://rubygems.org/
-gem install bundler
-
-source /home/vagrant/.bashrc
-source /home/vagrant/.profile
-
-# install the Gems
-cd /vagrant
-bundle install
-
 # configure PostgreSQL
 sudo -u postgres psql postgres -c "CREATE ROLE photastic LOGIN PASSWORD 'photastic' CREATEDB"
-rake db:create
-rake db:migrate
-
-# output IP address
-echo "Here is your IP address:"
-ip -4 -f inet addr
