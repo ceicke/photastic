05214d3d6af4011334d9103a95b07e6ceb92b48b
Christoph Eicke <christoph@geisterstunde.org>
2014-01-27 20:43:19 +0100

    WIP new layout

--- app/assets/javascripts/videos.js
+++ app/assets/javascripts/videos.js
@@ -1,7 +1,7 @@
 jQuery(function() {
   // this is for the overview of the video thumbnail that shows and hides the control area
-  $(".picture .picture_inner").find(".control_area").hide();
-  $(".picture .picture_inner").hover(
+  $(".picture_thumb .picture_inner").find(".control_area").hide();
+  $(".picture_thumb .picture_inner").hover(
     function(){
       $(this).find(".control_area").fadeIn(100);
     },function(){
