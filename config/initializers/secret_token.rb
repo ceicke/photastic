900b38475ddbd567649981952888afb47861bb2b
Christoph Eicke <christoph@geisterstunde.org>
2015-06-16 11:21:01 +0000

    updating to secrets.yml

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Photastic::Application.config.secret_token = 'cb0ff23fa822afa72f01bb03951e14122861f27e289773a79346218ce19bca146db829f507bc9452fd5266a404238aa495d62d009d830db7f2da0ccfad6af38e'
