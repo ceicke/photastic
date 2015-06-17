91c67335971418b72cf153dc146b2bd2ef696c84
Christoph Eicke <christoph@geisterstunde.org>
2015-06-17 06:33:06 +0000

    not saving things at S3 anymore

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,17 +66,17 @@ Photastic::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # the S3 configuration
-  AWS::S3::DEFAULT_HOST = "s3-eu-west-1.amazonaws.com"
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_protocol => 'https',
-    :s3_host_name => 's3-eu-west-1.amazonaws.com',
-    :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
+  #AWS::S3::DEFAULT_HOST = "s3-eu-west-1.amazonaws.com"
+  #config.paperclip_defaults = {
+  #  :storage => :s3,
+  #  :s3_protocol => 'https',
+  #  :s3_host_name => 's3-eu-west-1.amazonaws.com',
+  #  :s3_credentials => {
+  #    :bucket => ENV['AWS_BUCKET'],
+  #    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+  #    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  #  }
+  #}
 
   ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
